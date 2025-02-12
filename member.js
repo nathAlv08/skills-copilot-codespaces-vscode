9f47c68d075970d85b14c9f7526a5ac26148002e
nathAlv08 <150881129+nathAlv08@users.noreply.github.com>
2025-02-12 08:36:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember(){
+    console.log('Skill: Programming');
+    console.log('Skill: Design');
+    console.log('Skill: Data Science');
+    console.log('Skill: Machine Learning');
+}
